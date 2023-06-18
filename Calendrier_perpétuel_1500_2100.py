3f36eb25a3fcb59c45ee7a015b6a63e990076406
Dany Dubé <74633244+danydube1971@users.noreply.github.com>
2023-06-18 18:16:20 -0400

    Update Calendrier_perpétuel_1500_2100.py

--- Calendrier_perpétuel_1500_2100.py
+++ Calendrier_perpétuel_1500_2100.py
@@ -1,12 +1,11 @@
 """Le script permet à l'utilisateur d'entrer une année, un mois et un jour de début pour afficher le calendrier mensuel correspondant.
 
 La première partie du script vérifie que l'année, le mois et le jour de début sont valides en demandant à l'utilisateur d'entrer des valeurs numériques dans des plages spécifiées.
-
-La deuxième partie utilise le module calendar pour créer un objet TextCalendar avec le jour de début spécifié, puis utilise la méthode formatmonth pour afficher le calendrier mensuel de l'année et du mois spécifiés. Le script a été modifié pour permettre à l'utilisateur de choisir de commencer le calendrier par dimanche ou par lundi.
-
+La deuxième partie utilise le module calendar pour créer un objet TextCalendar avec le jour de début spécifié, puis utilise la méthode formatmonth 
+pour afficher le calendrier mensuel de l'année et du mois spécifiés. Le script a été modifié pour permettre à l'utilisateur de choisir de commencer le calendrier par dimanche ou par lundi.
 Le script a également été modifié pour afficher le calendrier en français, en utilisant la locale française configurée avec l'encodage UTF-8.
-
-En résumé, ce script permet à l'utilisateur de visualiser un calendrier mensuel pour une année et un mois donnés, avec un jour de début personnalisable et dans la langue de son choix (dans la limite des locales installées sur le système)."""
+En résumé, ce script permet à l'utilisateur de visualiser un calendrier mensuel pour une année et un mois donnés, avec un jour de début personnalisable 
+et dans la langue de son choix (dans la limite des locales installées sur le système)."""
 
 import calendar
 import locale
